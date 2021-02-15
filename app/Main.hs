76166d0323b0e70ba4c3a7878e34c96f0b8b3e1c
Axel Nilsson <axel.o.nilsson@gmail.com>
2021-02-15 17:19:42 +0100

    Added Network.HTTP library

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 -- Libraries
 import Data.Aeson
+import Network.HTTP (simpleHttp)
 
 main = do
     undefined
@@ -16,15 +17,14 @@ main = do
 
     When installing a library, use:
     ---------------------
-
     stack install <library>
     then add <library> under package.yaml dependencies
-    do not change .cabal manually, each library is added automatically
+    do not change .cabal manually, each library is added automatically by stack
 
 
     References: 
     ---------------------
-
     https://stackoverflow.com/questions/29941866/parsing-json-data-from-a-url-in-haskell-using-aeson
+    https://hackage.haskell.org/package/http-conduit-2.3.7.4/docs/Network-HTTP-Conduit.html
 
 -}
