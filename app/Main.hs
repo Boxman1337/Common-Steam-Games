4f0c12cdaf01bbd2df43683d2e18e209a869a6fa
Axel Nilsson <axel.o.nilsson@gmail.com>
2021-02-15 16:20:18 +0100

    Added Data.Aeson and instructions, rm'd Text.JSON

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,30 @@
--- stack ghci ./app/Main.hs
+module Main where
 
-import Text.JSON
+-- Libraries
+import Data.Aeson
+
+main = do
+    undefined
+
+{- 
+    Compile / Runtime Instructions:
+    ---------------------
+
+    stack build
+    stack ghci ./app/Main.hs
 
 
+    When installing a library, use:
+    ---------------------
+
+    stack install <library>
+    then add <library> under package.yaml dependencies
+    do not change .cabal manually, each library is added automatically
+
+
+    References: 
+    ---------------------
+
+    https://stackoverflow.com/questions/29941866/parsing-json-data-from-a-url-in-haskell-using-aeson
+
+-}
