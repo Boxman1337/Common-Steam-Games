a6498e3b9049d3bd890d262390685e9c62e527a7
Johan <johan.visteby@gmail.com>
2021-02-16 15:39:33 +0100

    testat.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,37 @@
 module Main where
 
 -- Libraries
+--import qualified Data.ByteString.Lazy as B
 import Data.Aeson
-import Network.HTTP (simpleHttp)
+import Network.HTTP
+
+data Games = Games {gameslist  :: String} 
+
+data DataPoint = DataPoint { response :: String
+                           , games :: String
+                           } deriving (Show)
+
+
+jsonURL = getSteam64 minSt
+
+
+
+minSt = "76561198046588035"
+
 
 main = do
-    undefined
+    putStrLn "Enter a steam64 id:"
+    stId <- getLine
+    if stId == "no"
+      then do
+        main
+        else putStrLn (getSteam64 stId)
+
+getSteam64  :: String -> String
+getSteam64 "" =  ""
+getSteam64 s = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=0786DE3A3F9117713096BAE4347B357A&steamid=" ++ s  ++  "&include_played_free_games=true&include_appinfo=true"
+
+
 
 {- 
     Compile / Runtime Instructions:
