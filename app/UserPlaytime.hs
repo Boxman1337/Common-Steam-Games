48d12d50583d9e931ec9742b6ed101ba56d56ee0
Johan <johan.visteby@gmail.com>
2021-03-03 10:08:27 +0100

    Added example to mapAliastoPlaytime

--- app/UserPlaytime.hs
+++ app/UserPlaytime.hs
@@ -16,6 +16,7 @@ import Data.List
    RETURNS: A list of tuples [(x,y)]
    SIDE-EFFECTS: -
    EXAMPLES:
+            mapAliasPlaytime [("Garry's Mod","88 hours"),("Plants vs. Zombies: Game of the Year","4 hours"),("Operation Flashpoint: Dragon Rising","47 hours"),("Aliens vs. Predator","77 hours"),("S.T.A.L.K.E.R.: Call of Pripyat","23 hours"),("Terraria","44 hours"),("Psychonauts","0 hours"),("LIMBO","0 hours"),("Amnesia: The Dark Descent","1 hours"),("Superbrothers: Sword & Sworcery EP","0 hours"),("Pox Nora","0 hours"),("Thinking with Time Machine","0 hours"),("Moonbase Alpha","0 hours"),("Gear Up","0 hours"),("Wolfenstein: The New Order","18 hours"),("Robocraft","0 hours"),("PAYDAY 2","0 hours")] ["Glaus"] == [("Garry's Mod","Glaus: 88 hours"),("Plants vs. Zombies: Game of the Year","Glaus: 4 hours"),("Operation Flashpoint: Dragon Rising","Glaus: 47 hours"),("Aliens vs. Predator","Glaus: 77 hours"),("S.T.A.L.K.E.R.: Call of Pripyat","Glaus: 23 hours"),("Terraria","Glaus: 44 hours"),("Psychonauts","Glaus: 0 hours"),("LIMBO","Glaus: 0 hours"),("Amnesia: The Dark Descent","Glaus: 1 hours"),("Superbrothers: Sword & Sworcery EP","Glaus: 0 hours"),("Pox Nora","Glaus: 0 hours"),("Thinking with Time Machine","Glaus: 0 hours"),("Moonbase Alpha","Glaus: 0 hours"),("Gear Up","Glaus: 0 hours"),("Wolfenstein: The New Order","Glaus: 18 hours"),("Robocraft","Glaus: 0 hours"),("PAYDAY 2","Glaus: 0 hours")]
 -}
 
 mapAliastoPlaytime :: [(String, String)] -> [String] -> [(String, String)]
