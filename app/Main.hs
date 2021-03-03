51137277c736a8a2eeb6146831a8ba2db21abb1e
Axel Nilsson <axel.o.nilsson@gmail.com>
2021-03-03 17:31:56 +0100

    Added more runtime instructions and a known error fix

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,50 @@ import GHC.IO.Encoding
 import Debug.Trace
 import Test.HUnit
 
+{- 
+    Compile / Runtime Instructions: How to run our software
+    ---------------------
+    stack build 
+    or
+    stack build --verbose
+        then
+            stack ghci ./app/Main.hs
+                then
+                    main
+
+
+
+    When installing a library, use:
+    ---------------------
+    stack install <library>
+    or 
+    cabal install <library> 
+        then
+            add <library> under package.yaml dependencies
+            (do not change .cabal manually, each library is added automatically by stack)
+
+
+
+    Run testcases by typing these lines in terminal:
+    ------------------------------------
+    stack ghci .\app\TestCases.hs
+        then
+            runtests
+
+    
+
+    Known errors and fix:
+    ----------------------
+    If you get the error:
+        "*** Exception: <stdout>: hPutChar: invalid argument (invalid character)" 
+        when running our software, it is not an error from our part, but we do have a temporary fix
+    Fix:
+        type: 
+            :q (if you are inside the ghci environment)
+                then
+                    chcp.com 65001 
+-}
+
 -- IO Functions
 
 {- main
@@ -151,38 +195,6 @@ getUsers (x:xs) = x ++ ", " ++ getUsers xs
 
 
 
-{- 
-    Compile / Runtime Instructions: 
-    ---------------------
-    stack build 
-    or
-    stack build --verbose
 
-    then
-
-    stack ghci ./app/Main.hs
-
-
-    When installing a library, use:
-    ---------------------
-    stack install <library>
-    or 
-    cabal install <library> 
-
-    then
-    
-    add <library> under package.yaml dependencies
-    (do not change .cabal manually, each library is added automatically by stack)
-
-
-    Run testcases by typing these lines in terminal:
-    ------------------------------------
-    stack ghci .\app\TestCases.hs
-
-    then
-
-    runtests
-
--}
 
 
